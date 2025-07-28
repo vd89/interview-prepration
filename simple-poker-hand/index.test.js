da491c995fbdf6e0f43f0d96e809e7fdbd1d9bdd
vd89 <dixitvora@gmail.com>
2025-07-28 23:34:54 +0530

    main: upated the tests to nodejs

--- simple-poker-hand/index.test.js
+++ simple-poker-hand/index.test.js
@@ -1,19 +1,21 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
 import { sum } from "./index.js";
 
 describe('test the sum function with positive numbers', () => {
   it('should return the sum of all numbers in the array', () => {
-    expect(sum([1, 2, 3, 4, 5])).toBe(15);
+    assert.strictEqual(sum([1, 2, 3, 4, 5]), 15);
   });
 });
 
 describe('test the sum function with negative numbers', () => {
   it('should return the sum of all numbers in the array', () => {
-    expect(sum([-1, -2, -3, -4, -5])).toBe(-15);
+    assert.strictEqual(sum([-1, -2, -3, -4, -5]), -15);
   });
 });
 
 describe('test the sum function with mixed numbers', () => {
   it('should return the sum of all numbers in the array', () => {
-    expect(sum([-1, 2, -3, 4, -5])).toBe(-3);
+    assert.strictEqual(sum([-1, 2, -3, 4, -5]), -3);
   });
 });
--- simple-poker-hand/jest.setup.js
+++
@@ -1,5 +0,0 @@
-// Enable automatic mocking for ESM modules
-import { jest } from '@jest/globals';
-
-// Make jest available globally
-global.jest = jest;
